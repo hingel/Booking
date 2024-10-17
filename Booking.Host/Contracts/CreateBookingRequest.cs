0c0958482159e8dfd8425a24384b4816b283e8fc
Henrik Ingelsten <112380774+hingel@users.noreply.github.com>
2024-10-17 10:41:25 +0200

    Add mediat r and Docker Support (#11)
    * AddedMediatR
    
    * - Added booking request
    
    * - deleted old folder
    
    * - added dockersupport
    - temporary removed functionality in createbooking handler
    
    * - updated
    
    ---------
    
    Co-authored-by: Henrik Ingelsten <henrik.ingelsten@jellyhive.se>

---
+++ Booking.Host/Contracts/CreateBookingRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Booking.Host.Contracts
+{
+	public record CreateBookingRequest(DateTimeOffset DateTime, int Duration, int Persons, ContactContract Contact, string TableId, string CompanyId);
+
+	public record ContactContract(string Name, string PhoneNumber, string Email);
+}
