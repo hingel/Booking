deef0c516b2028c80780ca3e3c486737fa034498
hingel <henrik.ingelsten@gmail.com>
2024-12-13 10:23:15 +0100

    - Added endpoint and handler for booking query

---
+++ Booking.Business/Query/Handlers/BookingQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Booking.Business.Mapping;
+using Booking.Contract.Responses;
+using Booking.DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NodaTime;
+
+namespace Booking.Business.Query.Handlers;
+
+public record BookingQuery(Guid CompanyId, LocalDate Fromdate) : IRequest<Result<BookingResponse[]>>;
+
+public class BookingQueryHandler(ApplicationDbContext dbContext) : IRequestHandler<BookingQuery, Result<BookingResponse[]>>
+{
+	public async Task<Result<BookingResponse[]>> Handle(BookingQuery request, CancellationToken cancellationToken)
+	{
+		var bookings = await dbContext.Tables
+			.Where(t => t.CompanyId == request.CompanyId)
+			.Select(t => t.Bookings.Where(b => b.DateTime.Date >= request.Fromdate))
+			.SelectMany(b => b)
+			.ToArrayAsync(cancellationToken);
+
+		return bookings.Length > 0 ? new Result<BookingResponse[]>(
+			true,
+			$"Booking for company: {request.CompanyId} from {request.Fromdate}", 
+			bookings.Select(b => b.ToContract()).ToArray()) :
+			new Result<BookingResponse[]>(false, "no bookings found", null);
+	}
+}
